0836d73fb27aea14d19a35727301f8cc44767bb4
rahmanhas <rahmanhas@gmail.com>
2023-06-08 23:13:14 +0600

    jwt generated and connection to client is established

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const jwt =require('jsonwebtoken')
 app.use(cors())
 app.use(express.json())
 
@@ -18,11 +19,48 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
+  //verify jwt from client to server
+  const verifyJWT = (req,res,next)=>{
+    const authorization = req.headers.authorization;
+    console.log(authorization);
+    //token verify
+
+
+
+    next()
+  }
 
   async function run() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+      //await client.connect();
+      const usersCollection = client.db('dance-flow').collection('users');
+
+
+      //generate jwt token
+      app.post('/jwt',async(req,res)=>{
+        const email = req.body;
+        const token = jwt.sign(email,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '7d',})
+        res.send({token})
+      })
+
+      //save user email in mongodb when user is created in client side
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exists' })
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+
+
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
